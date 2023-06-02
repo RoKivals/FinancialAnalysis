8bf16b62d1034e0b168d28f1f2caddd063047a1a
RoKivals <slava111103@gmail.com>
2023-06-02 09:00:24 +0300

    WIP

--- Algorithms and DS/HashTable.py
+++ Algorithms and DS/HashTable.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple, Union
+
+
 class Node:
     def __init__(self, key, value):
         self.key = key
@@ -6,16 +9,35 @@ class Node:
 
 
 class HashTable:
-    def __init__(self, capacity):
+    def __init__(self, capacity: int = 1000):
         self.capacity = capacity
         self.size = 0
-        self.table = [None] * capacity
+        self.table: List[Union[None, Node]] = [None] * capacity
 
-    def _hash(self, key):
-        return hash(key) % self.capacity
+    # Метод деления
+    def _hash_key(self, key):
+        res = 0
+        if isinstance(key, (int, float)):
+            res = int(key % self.capacity)
+        elif isinstance(key, str):
+            summa = 0
+            for cnt, sym in enumerate(key):
+                summa += ord(sym) * cnt
+                res = summa % self.capacity
+        elif isinstance(key, (List[str, int, float], Tuple[str, int, float])):
+            summa = 0
+            for elem in key:
+                summa += hash(elem)
+                res = summa % self.capacity
+        else:
+            try:
+                hash(key) % self.capacity
+            except TypeError:
+                raise TypeError("Этот объект нельзя хэшировать")
+        return res
 
     def insert(self, key, value):
-        index = self._hash(key)
+        index = self._hash_key(key)
 
         if self.table[index] is None:
             self.table[index] = Node(key, value)
@@ -32,19 +54,18 @@ class HashTable:
             self.table[index] = new_node
             self.size += 1
 
-    def search(self, key):
-        index = self._hash(key)
+    def _search(self, key):
+        index = self._hash_key(key)
 
         current = self.table[index]
         while current:
             if current.key == key:
                 return current.value
             current = current.next
-
-        raise KeyError(key)
+        raise KeyError(key, "такого ключа нет")
 
     def remove(self, key):
-        index = self._hash(key)
+        index = self._hash_key(key)
 
         previous = None
         current = self.table[index]
@@ -62,15 +83,43 @@ class HashTable:
 
         raise KeyError(key)
 
+    def longest_chain(self):
+        max_len = 0
+        for elem in self.table:
+            if elem is not None:
+                length = 1
+                current = elem
+                while current.next:
+                    length += 1
+                    current = current.next
+                max_len = max(max_len, length)
+        return max_len
+
+    def shortest_chain(self):
+        max_len = self.size
+        for elem in self.table:
+            if elem is not None:
+                length = 1
+                current = elem
+                while current.next:
+                    length += 1
+                    current = current.next
+                max_len = max(max_len, length)
+        return max_len
+
     def __len__(self):
         return self.size
 
     def __contains__(self, key):
         try:
-            self.search(key)
+            self._search(key)
             return True
         except KeyError:
             return False
+
+
+def main():
+    pass
 
 
 # Driver code
